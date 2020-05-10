a6fea5efc78abd4038a9acd47d852087be121e35
Elisha Schwarz <eschwarz0@gmail.com>
2020-05-10 13:34:23 -0400

    Inital Project Setup
    Set up project as ASP.NET Core 3 MVC. Add in basic data layer

---
+++ Luna2.Data/dbo/Tables/AnimalType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[AnimalType]
+(
+	[Id] INT Identity (100,1) NOT NULL PRIMARY KEY
+	,[Description] varchar(255) not null
+	,[DateCreated] datetime2 not null
+)
---
+++ Luna2.Data/dbo/Tables/Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id] INT IDENTITY(100,1) NOT NULL PRIMARY KEY
+	,[FirstName] varchar(255) not null
+	,[MiddleInitial] varchar(5) not null
+	,[LastName] varchar(255) NOT NULL
+	,[DateCreated] datetime2 not null
+)
