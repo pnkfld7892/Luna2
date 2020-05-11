8dfacf01671f4a22f73a5dcd152d3e103bb67b55
Elisha Schwarz <eschwarz0@gmail.com>
2020-05-10 20:09:02 -0400

    Change To Animal Person Linking Table
    Change the data model to use linking table between person and animal to keep the data clean.

--- Luna2.Data/dbo/Tables/Animal.sql
+++ Luna2.Data/dbo/Tables/Animal.sql
@@ -3,8 +3,6 @@
 	[Id] INT identity(100,1) NOT NULL PRIMARY KEY
 	,[AnimalName] varchar(255) not null
 	,[AnimalTypeId] int not null
-	,[PersonId] int not null
 	,[DateOfBirth] datetime2 not null
     ,CONSTRAINT [FK_Animal_ToAnimalType] FOREIGN KEY (AnimalTypeId) REFERENCES [AnimalType]([Id])
-	,CONSTRAINT [FK_Animal_ToPerson] FOREIGN KEY (PersonId) REFERENCES [PERSON]([Id])
 )
---
+++ Luna2.Data/dbo/Tables/Animal_x_Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Animal_x_Person]
+(
+	[Id] INT identity(100,1) NOT NULL PRIMARY KEY
+	,AnimalId int not null
+	,PersonId int not null, 
+    CONSTRAINT [FK_Animal_x_Person_ToAnimal] FOREIGN KEY (AnimalId) REFERENCES Animal(Id) 
+    ,CONSTRAINT [FK_Animal_x_Person_ToPerson] FOREIGN KEY (PersonId) REFERENCES Person(Id)
+
+)
